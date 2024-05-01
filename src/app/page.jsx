782b495481e4a0c91b0bba130aba9d5e92aa278f
Casper Banemann <casperbanemann@gmail.com>
2024-05-01 14:37:45 +0200

    css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-
+import styles from './styles.module.css'; // Note the updated import path
 
 export const metadata = {
   title: "Home",
@@ -8,26 +8,24 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-100">
+    <main className={styles.fullHeight}>
       <Header/>
 
-      <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4 text-black">Diversa</h1>
+      <div className={styles.contentContainer}>
+        <h1 className={`${styles.heading} ${styles.largeText}`}>Diversa</h1>
 
-        <form  action="/report" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="url">
+        <form action="/report" className={styles.formContainer}>
+          <div className={styles.inputGroup}>
+            <label className={styles.formLabel} htmlFor="url">
               URL
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="url" name="url" placeholder="Indtast URL" required></input>
+            <input className={styles.inputField} type="text" id="url" name="url" placeholder="Indtast URL" required></input>
           </div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          <button className={styles.submitButton} type="submit">
             Check it
           </button>
         </form>
-
       </div>
     </main>
   );
 }
-

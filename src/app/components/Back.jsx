97fbd45d80ae99470da00538773adebed05a70c8
Casper Banemann <casperbanemann@gmail.com>
2024-05-03 00:07:13 +0200

    back component

---
+++ src/app/components/Back.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import styles from './Back.module.css';
+
+
+const Back = () => {
+  return (
+    <main className={styles.backMain}>
+       <Link className={styles.backArrow} prefetch={false} href="/">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
+        </svg>
+      </Link>
+    </main>
+  );
+};
+
+export default Back;
